4cdcc0a21ef9ebe56349ec10fd527dc804941efd
15927249176 <961924346@qq.com>
2020-08-04 01:45:56 +0800

    增加用户异常值

--- src/main/java/com/security/admin/exception/UmsAdminExcetionEnum.java
+++ src/main/java/com/security/admin/exception/UmsAdminExcetionEnum.java
@@ -1,8 +1,10 @@
 package com.security.admin.exception;
 
 public enum UmsAdminExcetionEnum {
-    USER_EXIST("001","用户已存在"),
-    USER_NAME_PASS_ERROR("002","用户名和密码错误");
+    USER_NOT_LOGIN("001","用户未登录"),
+    USER_EXIST("002","用户已存在"),
+    USER_NAME_PASS_ERROR("003","用户名和密码错误"),
+    USER_NOT_AUTH("004","该用户无此权限，请联系管理员");
 
     private String code;
     private String message;
